d47297e6616a50abac534e68a19bd7e35aa94e53
Fayzan Ali Akhtar <fayzan585@gmail.com>
2023-08-29 17:36:56 +0500

    Made UI Changes to Teams

--- Frontend/src/Container/Feed/FeedComponents/Team.tsx
+++ Frontend/src/Container/Feed/FeedComponents/Team.tsx
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from 'react';
-import Spinner from 'react-bootstrap/Spinner';
-import {User,Project} from '../../../Constants/Constants'
+import React, { useState, useEffect } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { User, Project } from "../../../Constants/Constants";
 import { fetchOneProjectFromServer } from "../../../Services/Project/GetOneProject";
 import { fetchAllDevelopersFromServer } from "../../../Services/Developer/GetAllDevelopers";
 import { fetchAllQAsFromServer } from "../../../Services/QA/GetAllQAs";
@@ -12,159 +12,176 @@ interface Props {
 // Get all the developers obj and QAs obj
 // then filter which are in team and not in team
 
-const Team: React.FC<Props> = ({projectID}) => {
-  // Project Data 
+const Team: React.FC<Props> = ({ projectID }) => {
+  // Project Data
   const [project, setProject] = useState<Project>();
-  // Loading State 
+  // Loading State
   const [isLoading, setIsLoading] = React.useState(true);
-  // Developers 
+  // Developers
   const [teamDevelopers, setTeamDevelopers] = useState<User[]>([]);
   const [availableDevelopers, setAvailableDevelopers] = useState<User[]>([]);
-  // QAs 
+  // QAs
   const [availableQAs, setAvailableQAs] = useState<User[]>([]);
   const [teamQAs, setTeamQAs] = useState<User[]>([]);
-  
 
-  useEffect(() => {
-    // Fetch and set the developer and QA data here
-    // For example:
-    // setAllDevelopers([...arrayOfDevelopers]);
-    // setAllQAs([...arrayOfQAs]);
-    // setIsLoading(false); // After fetching data, set isLoading to false
-  }, []);
-
-  // const fetchProjects = async () => {
-  //   try {
-  //     // const ProjectsData = await fetchAllProjectsFromServer(projectID);
-
-  //     if (ProjectsData.length === 0) {
-  //       // console.log("No Projects");
-  //       // setMessage("No Projects")
-  //       // sethasProject(false);
-  //     } else {
-  //       // sethasProject(true);
-  //       // setProjects(ProjectsData);
-  //       for (const project of ProjectsData) {
-  //         console.log(project);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching projects:", error);
-  //   }
-  // };
-
-  async function getAndSetDevQaData(){
-    console.log("getAndSetDevQaData");
-    try
-    {
-
+  async function getAndSetDevQaData() {
+    try {
+      // Loading Started
+      setIsLoading(true);
+      // Getting Project Data fro Server
       const projectData = await fetchOneProjectFromServer(projectID);
-      // console.log("projectData");
-      // console.log(projectData);
       setProject(projectData);
+      // Getting All Developers from Server
       const allDevelopersData = await fetchAllDevelopersFromServer();
-      // console.log("allDevelopersData");
-      // console.log(allDevelopersData);
+      // Getting All QAs from Server
       const allQAsData = await fetchAllQAsFromServer();
-      // console.log("allQAsData");
-      // console.log(allQAsData);
-      // Separeting Team Developers 
-      const developersInTeam = allDevelopersData.filter((developer) => 
-      {
-         if(projectData.developers.includes(developer.id))
-         {
-            return developer;
-         }
+      // Separeting Team Developers
+      const developersInTeam = allDevelopersData.filter((developer) => {
+        if (projectData.developers.includes(developer.id)) {
+          return developer;
         }
-      );
-      // Separeting Available Developers 
-      const developersAvailable = allDevelopersData.filter((developer) => 
-      {
-         if(!projectData.developers.includes(developer.id))
-         {
-            return developer;
-         }
+      });
+      // Separeting Available Developers
+      const developersAvailable = allDevelopersData.filter((developer) => {
+        if (!projectData.developers.includes(developer.id)) {
+          return developer;
         }
-      );
+      });
       // Separating Team QAs
-      const QAsInTeam = allQAsData.filter((qa) => 
-      {
-         if(projectData.qas.includes(qa.id))
-         {
-            return qa;
-         }
+      const QAsInTeam = allQAsData.filter((qa) => {
+        if (projectData.qas.includes(qa.id)) {
+          return qa;
         }
-      );
+      });
       // Separating Available QAs
-      const QAsAvailable = allQAsData.filter((qa) => 
-      {
-         if(!projectData.qas.includes(qa.id))
-         {
-            return qa;
-         }
+      const QAsAvailable = allQAsData.filter((qa) => {
+        if (!projectData.qas.includes(qa.id)) {
+          return qa;
         }
-      );
-        // Setting States 
-        setTeamDevelopers(developersInTeam);
-        setAvailableDevelopers(developersAvailable);
-        setTeamQAs(QAsAvailable);
-        setAvailableQAs(QAsAvailable);
-        setIsLoading(false);
-        setIsLoading(false)
-
-    }
-    catch(error)
-    {
+      });
+      // Setting States
+      setTeamDevelopers(developersInTeam);
+      console.log("developersInTeam : ", developersInTeam);
+      setTeamDevelopers(developersInTeam);
+      setAvailableDevelopers(developersAvailable);
+      setTeamQAs(QAsAvailable);
+      setAvailableQAs(QAsAvailable);
+      // Loading finished
+      setIsLoading(false);
+    } catch (error) {
       console.log(`Error in getAndSetDevQaData: ${error}`);
     }
-    // Get all the developers obj and QAs obj
-    // then filter which are in team and not in team
-    // For example:
-    // setAllDevelopers([...arrayOfDevelopers]);
-    // setAllQAs([...arrayOfQAs]);
-    // setIsLoading(false); // After fetching data, set isLoading to false
   }
 
-  useEffect(() => {   
-    getAndSetDevQaData();
+  function removeDeveloperFromTeam() {
+    console.log("Remove Developer from Team");
+  }
+
+  useEffect(() => {
     getAndSetDevQaData();
   }, []);
 
   return (
     <>
-    <div className="d-flex justify-content-center align-items-center">
-      {isLoading && (
+    <div className="d-flex justify-content-center align-items-center  mt-2">
+      {isLoading ? (
         <Spinner animation="grow" variant="success" />
+      ) : (
+        <div className="w-100">
+          {/* Team Developers */}
+          <div className="w-100 border-top border-bottom border-success pt-1">
+            <h2 className="text-center mb-4">Team Developers</h2>
+            <div className="d-flex flex-column align-items-center">
+              {teamDevelopers.map((developer) => (
+                <div
+                  key={developer.id}
+                  className="d-flex justify-content-between align-items-center w-100 mb-3"
+                >
+                  <div>
+                    {developer.firstName} {developer.lastName}
+                  </div>
+                  <button
+                    className="btn btn-danger"
+                    onClick={removeDeveloperFromTeam}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Team QAs */}
+          <div className="w-100 border-top border-bottom border-success pt-1">
+            <h2 className="text-center mb-4">Team QAs</h2>
+            <div className="d-flex flex-column align-items-center">
+              {teamQAs.map((qa) => (
+                <div
+                  key={qa.id}
+                  className="d-flex justify-content-between align-items-center w-100 mb-3"
+                >
+                  <div>
+                    {qa.firstName} {qa.lastName}
+                  </div>
+                  <button
+                    className="btn btn-danger"
+                    onClick={removeDeveloperFromTeam}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Available Developers */}
+          <div className="w-100 border-top border-bottom border-success pt-1">
+            <h2 className="text-center mb-4">Available Developers</h2>
+            <div className="d-flex flex-column align-items-center">
+              {availableDevelopers.map((developer) => (
+                <div
+                  key={developer.id}
+                  className="d-flex justify-content-between align-items-center w-100 mb-3"
+                >
+                  <div>
+                    {developer.firstName} {developer.lastName}
+                  </div>
+                  <button
+                    className="btn btn-success"
+                    onClick={removeDeveloperFromTeam}
+                  >
+                    Add
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Available QAs */}
+          <div className="w-100 border-top border-bottom border-success pt-1">
+            <h2 className="text-center mb-4">Available QAs</h2>
+            <div className="d-flex flex-column align-items-center">
+              {availableQAs.map((qa) => (
+                <div
+                  key={qa.id}
+                  className="d-flex justify-content-between align-items-center w-100 mb-3"
+                >
+                  <div>
+                    {qa.firstName} {qa.lastName}
+                  </div>
+                  <button
+                    className="btn btn-success"
+                    onClick={removeDeveloperFromTeam}
+                  >
+                    Add
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       )}
     </div>
-    <div className="d-flex flex-column align-items-center">
-        {/* <h1>Team Tab</h1> */}
-
-        {/* Displaying Team Developers */}
-        <h2>Team Developers</h2>
-        {teamDevelopers.map((developer) => (
-          <div key={developer.id}>{developer.firstName}</div>
-        ))}
-
-        {/* Displaying Team QAs */}
-        <h2>Team QAs</h2>
-        {teamQAs.map((qa) => (
-          <div key={qa.id}>{qa.firstName}</div>
-        ))}
-
-        {/* Displaying Available Developers */}
-        <h2>Available Developers</h2>
-        {availableDevelopers.map((developer) => (
-          <div key={developer.id}>{developer.firstName}</div>
-        ))}
-
-        {/* Displaying Available QAs */}
-        <h2>Available QAs</h2>
-        {availableQAs.map((qa) => (
-          <div key={qa.id}>{qa.firstName}</div>
-        ))}
-      </div>
-
   </>
   );
 };
